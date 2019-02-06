12ac04bbfd06e646d7e73906d8b96af97887de91
Ron Richardson <ron.richardson@gmail.com>
2019-02-06 16:42:05 -0500

    UI update for columns and firebase deploy

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const Timer = require('easytimer');
-//window.$ = window.jQuery = require('jquery')
 
 /** @const */
 const appOpts = {
@@ -45,7 +44,7 @@ const createTimer = (seconds, el) => {
     });
     timer.addEventListener('targetAchieved', function(e) {
 		timer.stop();
-		//createTimer(seconds, el);
+		createTimer(seconds, el);
 	});
 }
 
@@ -54,83 +53,4 @@ startWakeLock();
 startTimers();
 
 
-    /*"dist": "yarn clean && yarn copy && yarn build:js && yarn build:sw && yarn build:inline:css && yarn build:inline:js && yarn build:minify && yarn build:smaller",*/
-
-
-
-
-
-
-
-
-
-// document.querySelector('#show-mph').addEventListener('click', (event) => {
-//   appOpts.readoutUnit = readoutUnits.mph;
-//   if (!appOpts.dom.showMph.classList.contains('selected')) {
-//     toggleReadoutButtons();
-//   }
-// });
-
-// document.querySelector('#show-kmh').addEventListener('click', (event) => {
-//   appOpts.readoutUnit = readoutUnits.kmh;
-//   if (!appOpts.dom.showKmh.classList.contains('selected')) {
-//     toggleReadoutButtons();
-//   }
-// });
-
-// document.querySelector('#start').addEventListener('click', (event) => {
-//   if (appOpts.watchId) {
-//     navigator.geolocation.clearWatch(appOpts.watchId);
-
-//     if (appOpts.wakeLock) {
-//       appOpts.wakeLock.cancel();
-//     }
-
-//     appOpts.watchId = null;
-//     appOpts.dom.start.textContent = '🔑 Start';
-//     appOpts.dom.start.classList.toggle('selected');
-//   } else {
-//     const options = {
-//       enableHighAccuracy: true
-//     };
-//     appOpts.watchId = navigator.geolocation.watchPosition(parsePosition,
-//       null, options);
-//     startWakeLock();
-
-//     appOpts.dom.start.textContent = '🛑 Stop';
-//     appOpts.dom.start.classList.toggle('selected');
-//   }
-// });
-
-// const toggleReadoutButtons = () => {
-//   appOpts.dom.showKmh.classList.toggle('selected');
-//   appOpts.dom.showMph.classList.toggle('selected');
-// };
-
-// const startAmbientSensor = () => {
-//   if ('AmbientLightSensor' in window) {
-//     navigator.permissions.query({ name: 'ambient-light-sensor' })
-//       .then(result => {
-//         if (result.state === 'denied') {
-//           return;
-//         }
-//         const sensor = new AmbientLightSensor({frequency: 0.25});
-//         sensor.addEventListener('reading', () => {
-//           if (sensor['illuminance'] < 3 && !appOpts.dom.body.classList.contains('dark')) {
-//             appOpts.dom.body.classList.toggle('dark');
-//           } else if (sensor['illuminance'] > 3 && appOpts.dom.body.classList.contains('dark')) {
-//             appOpts.dom.body.classList.toggle('dark');
-//           };
-//         });
-//         sensor.start();
-//     });
-//   }
-// }
-
-
-
-// const parsePosition = (position) => {
-//   appOpts.dom.readout.textContent = Math.round(
-//     position.coords.speed * appOpts.readoutUnit);
-// };
-//startAmbientSensor();
+/*"dist": "yarn clean && yarn copy && yarn build:js && yarn build:sw && yarn build:inline:css && yarn build:inline:js && yarn build:minify && yarn build:smaller",*/
